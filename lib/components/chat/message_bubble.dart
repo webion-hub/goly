c3a4126fa10dd54a34f6e5adc092ecf118c2672e
Alessandro Dodi <dodialessandro3@gmail.com>
2023-02-08 14:35:22 +0100

    Add userame in chat

--- lib/components/chat/message_bubble.dart
+++ lib/components/chat/message_bubble.dart
@@ -13,7 +13,12 @@ class MessageBubble extends StatelessWidget {
         Container(
           decoration: BoxDecoration(
             color:  isMe ? Theme.of(context).focusColor : Theme.of(context).focusColor.withAlpha(10),
-            borderRadius: BorderRadius.circular(12),
+            borderRadius: BorderRadius.only(
+              topLeft: const Radius.circular(12),
+              topRight: const Radius.circular(12),
+              bottomLeft: isMe ? const Radius.circular(12) : const Radius.circular(0),
+              bottomRight: isMe ? const Radius.circular(0) : const Radius.circular(12),
+            ),
           ),
           width: 140,
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
--- lib/components/chat/messages.dart
+++ lib/components/chat/messages.dart
@@ -8,26 +8,37 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: FirebaseFirestore.instance
-          .collection('chat')
-          .orderBy('dateTime', descending: true)
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        return ListView.builder(
-          itemCount: snapshot.data?.docs.length ?? 0,
-          reverse: true,
-          itemBuilder: ((context, index) => MessageBubble(
-                text: snapshot.data?.docs[index]['text'],
-                isMe: snapshot.data?.docs[index]['userId'] == FirebaseAuth.instance.currentUser!.uid,
-              )),
-        );
-      },
+    return Column(
+      children: [
+        FutureBuilder(
+            future:
+                FirebaseFirestore.instance.collection('users').doc('PKhYVNZOFUXcJkNva7NWN4MEcs53').get(),
+            builder: ((context, snapshot) => Text(snapshot.data!['username']))),
+        Expanded(
+          child: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection('chat')
+                .orderBy('dateTime', descending: true)
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return ListView.builder(
+                itemCount: snapshot.data?.docs.length ?? 0,
+                reverse: true,
+                itemBuilder: ((context, index) => MessageBubble(
+                      text: snapshot.data?.docs[index]['text'],
+                      isMe: snapshot.data?.docs[index]['userId'] ==
+                          FirebaseAuth.instance.currentUser!.uid,
+                    )),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
