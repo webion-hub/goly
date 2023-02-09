8a166f4ed8848e7d872d7b709b4ebb3f2993d9a4
Alessandro Dodi <dodialessandro3@gmail.com>
2023-02-09 16:22:23 +0100

    Fix message and settings  models

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,18 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Message {
-  String? content;
-  String? senderUid;
-  String? messageId;
-  Timestamp? time;
-  Message({this.content, this.senderUid, this.messageId});
+  String content;
+  String senderUid;
+  String messageId;
+  Timestamp time;
+  Message({required this.content, required this.senderUid, required this.messageId, required this.time,});
 
-  Message.fromJson(Map<String, dynamic> json) {
-    content = json['content'];
-    senderUid = json['senderUid'];
-    messageId = json['messageId'];
+  Message.fromJson(Map<String, dynamic> json):
+    content = json['content'],
+    senderUid = json['senderUid'],
+    messageId = json['messageId'],
     time = json['time'];
-  }
+  
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
--- lib/models/settings.dart
+++ lib/models/settings.dart
@@ -1,18 +1,19 @@
 class SettingsModel  {
   bool? privateGoalsByDefault;
+  bool? privateDescriptionsByDefault;
   bool? privateAccount;
 
-  SettingsModel({this.privateGoalsByDefault, this.privateAccount});
+  SettingsModel({this.privateGoalsByDefault = true, this.privateDescriptionsByDefault = true, this.privateAccount = false});
 
-    SettingsModel.fromJson(Map<String, dynamic> json) {
-    privateGoalsByDefault = json['privateGoalsByDefault'];
+    SettingsModel.fromJson(Map<String, dynamic> json) :
+    privateGoalsByDefault = json['privateGoalsByDefault'],
     privateAccount = json['privateAccount'];
-  }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
-    data['privateGoalsByDefault'] = privateGoalsByDefault;
-    data['privateAccount'] = privateAccount;
+    data['privateGoalsByDefault'] = privateGoalsByDefault ?? 'true';
+    data['privateDescriptionsByDefault'] = privateDescriptionsByDefault ?? 'true';
+    data['privateAccount'] = privateAccount ?? 'false';
     return data;
   }
 }
