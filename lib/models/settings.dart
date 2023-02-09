1fe1936c378fe49d0478f52fdb574fac59263c07
Alessandro Dodi <dodialessandro3@gmail.com>
2023-02-09 15:47:20 +0100

    -Settings model fixed

--- lib/models/settings.dart
+++ lib/models/settings.dart
@@ -1,4 +1,18 @@
 class SettingsModel  {
   bool? privateGoalsByDefault;
   bool? privateAccount;
+
+  SettingsModel({this.privateGoalsByDefault, this.privateAccount});
+
+    SettingsModel.fromJson(Map<String, dynamic> json) {
+    privateGoalsByDefault = json['privateGoalsByDefault'];
+    privateAccount = json['privateAccount'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['privateGoalsByDefault'] = privateGoalsByDefault;
+    data['privateAccount'] = privateAccount;
+    return data;
+  }
 }
