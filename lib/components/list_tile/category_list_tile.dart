9ded40f44617d73d19da5d0cac4c895df9651811
Alessandro Dodi <dodialessandro3@gmail.com>
2023-02-16 14:50:38 +0100

    fixes

--- lib/components/list_tile/category_list_tile.dart
+++ lib/components/list_tile/category_list_tile.dart
@@ -2,12 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:goly/models/category.dart';
 import 'package:goly/pages/main/goals/actions/category/category_page.dart';
+import 'package:goly/utils/utils.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class CategoryCard extends StatelessWidget {
   final CategoryModel category;
   const CategoryCard({super.key, required this.category});
-
+  void noAction(_) {}
+  void yesAction(_) {}
   @override
   Widget build(BuildContext context) {
     return Center(
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:goly/utils/firebase.dart';
 
@@ -8,14 +10,41 @@ class Utils {
     final snackBar = SnackBar(content: Text(text));
 
     messangerKey.currentState!
-    ..removeCurrentSnackBar()
-    ..showSnackBar(snackBar);
+      ..removeCurrentSnackBar()
+      ..showSnackBar(snackBar);
   }
+
   static String currentUid() {
     return firebaseAuth.currentUser!.uid;
   }
 
-    static String currentEmail() {
+  static String currentEmail() {
     return firebaseAuth.currentUser!.email!;
+  }
+
+  static Future showCustomDialog({
+    required String title,
+    required String message,
+    required Function noAction,
+    required Function yesAction,
+    required BuildContext context,
+  }) {
+    return showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+              title: Text(title),
+              content: Text(message
+              ),
+              actions: <Widget>[
+                ElevatedButton(
+                  child: const Text('No'),
+                  onPressed: () => noAction(),
+                ),
+                ElevatedButton(
+                  child: const Text('Yes'),
+                  onPressed: () => yesAction(),
+                ),
+              ],
+            ));
   }
 }
