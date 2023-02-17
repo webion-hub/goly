f119f331c18908026000dddf1a76b99ec2e14af4
Alessandro Dodi <dodialessandro3@gmail.com>
2023-02-17 22:08:03 +0100

    Add responsive layout components

---
+++ lib/responsive/mobile_screen_layout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MobileScreenlayout extends StatelessWidget {
+  const MobileScreenlayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('mobile screen layout'),
+    );
+  }
+}
