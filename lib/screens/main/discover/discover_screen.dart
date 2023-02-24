10d03a9bf8d2d0bd35876d6c589cfa66f69be700
Alessandro Dodi <dodialessandro3@gmail.com>
2023-02-24 17:29:24 +0100

    Bugfix

--- lib/screens/main/discover/discover_screen.dart
+++ lib/screens/main/discover/discover_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:goly/models/post.dart';
 import 'package:goly/services/post_service.dart';
+import 'package:goly/utils/constants.dart';
 import 'package:goly/widgets/cards/post_card.dart';
 import 'package:goly/widgets/layout/app_bars/discover_app_bar.dart';
 import 'package:goly/widgets/layout/indicators.dart';
@@ -13,23 +14,26 @@ class DiscoverScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const DiscoverAppBar(),
-      body: SingleChildScrollView(
-        child: StreamBuilder(
-          stream: PostService.getPostStream(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return customBuffering();
-            }
-            if (snapshot.data == null) {
-              return const Text('Your feed is empty');
-            }
-            return Column(children: [
-              ...?snapshot.data?.docs.map((e) => PostCard(
-                    post: PostModel.fromJson(e.data() as Map<String, dynamic>),
-                  ))
-            ]);
-          },
-        ),
+      body: StreamBuilder(
+        stream: PostService.getPostStream(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return customBuffering();
+          }
+          if (snapshot.data == null) {
+            return const Text('Your feed is empty');
+          }
+          return ListView.builder(
+            padding: Constants.pagePadding,
+            itemBuilder: ((context, index) {
+              return PostCard(
+                post: PostModel.fromJson(
+                    snapshot.data?.docs[index].data() as Map<String, dynamic>),
+              );
+            }),
+            itemCount: snapshot.data!.size,
+          );
+        },
       ),
     );
   }
