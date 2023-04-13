15924b0cbdabb06594ca12f465c121a384036dc2
AlessandroDodi <dodialessandro3@gmail.com>
2023-04-13 10:31:46 +0200

    Refactor

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -9,14 +9,10 @@ import 'package:uuid/uuid.dart';
 class Constants {
   static String appName = "Goly";
   static String youtubeAPiKey = 'AIzaSyAZWBqMHNjwcDZ7IDd15UDcOkSlXsAflDE';
-  static String userImageDefault =
-      'https://firebasestorage.googleapis.com/v0/b/goly-app.appspot.com/o/user_image%2Fuser.jpeg?alt=media&token=3bb9a06b-fdde-42a7-aba0-1e570bed7899';
+  static String userImageDefault = 'https://firebasestorage.googleapis.com/v0/b/goly-app.appspot.com/o/user_image%2Fuser.jpeg?alt=media&token=3bb9a06b-fdde-42a7-aba0-1e570bed7899';
 
-  static const pagePadding =
-      EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0);
-
+  static const pagePadding = EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0);
   static const webScreenSize = 600;
-
   static final List<Widget> homeScreenItems = [
     const FriendsScreen(),
     const GoalsScreen(),
