475505d93e52d44d1f791c940d9d6bd6fc59d660
Alessandro <96247758+AlessandroDodi@users.noreply.github.com>
2023-04-25 11:45:56 +0200

    Remove files (#20)

--- lib/widgets/dialogs/loading_dialog.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:goly/widgets/layout/indicators.dart';
-
-void loadingDialog(BuildContext context) {
-  showDialog(context: context, barrierDismissible: false, builder: (context) => buffering());
-}
