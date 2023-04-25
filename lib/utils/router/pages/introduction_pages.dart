63b0f62011f568966172ec0d3a5db1910ee5d4db
AlessandroDodi <dodialessandro3@gmail.com>
2023-04-25 11:32:19 +0200

    Remove files

--- lib/utils/router/pages/introduction_pages.dart
+++
@@ -1,9 +0,0 @@
-import 'package:go_router/go_router.dart';
-import 'package:goly/screens/introductions/explenation_screen.dart';
-
-final List<RouteBase> introductionPages = [
-  GoRoute(
-    path: ExplenationScreen.routeName,
-    builder: (context, state) => const ExplenationScreen(),
-  ),
-];
